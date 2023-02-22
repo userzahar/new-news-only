d36ab7c7d5d2e536c4cb86055084b38b2497cc0e
Anna <100880lar@gmail.com>
2023-02-22 23:29:15 +0200

    fix btn add to favorite

--- src/favorite.js
+++ src/favorite.js
@@ -1,21 +1,27 @@
 import { refs } from './js/refs';
-import { favoritesInLocalStorage } from './js/localStorageFavorite';
+import {
+  favoritesInLocalStorage,
+  favoriteGalleryList,
+} from './js/localStorageFavorite';
 // console.log(favoritesInLocalStorage);
-
 
 refs.errorFind.classList.remove('notfind-part-hidden');
 
-if (
-  window.location.pathname === '/favorite.html'
-) { 
+if (window.location.pathname === '/favorite.html') {
+  function renderFavorites(element, constMarkup) {
+    refs.errorFind.classList.add('notfind-part-hidden');
+    element.insertAdjacentHTML('beforeend', constMarkup);
+  }
 
-function renderFavorites(element, constMarkup) {
-  refs.errorFind.classList.add('notfind-part-hidden');
-  element.insertAdjacentHTML('beforeend', constMarkup);
-}
-
-function createMarkupFav({ title, img, description, source, category, date }) {
-  return `<li class="gallery__item">
+  function createMarkupFav({
+    title,
+    img,
+    description,
+    source,
+    category,
+    date,
+  }) {
+    return `<li class="gallery__item">
     <article class="gallery__article">
               <div class="gallery__thumb"> <p class="gallery__category">${category}</p>
                 <img class="gallery__img" src="${img}" alt=""/>
@@ -36,59 +42,58 @@ function createMarkupFav({ title, img, description, source, category, date }) {
                     </div>
                 </article>
              </li>`;
-}
-
-favoritesInLocalStorage.map(el => {
-  renderFavorites(refs.galleryList, createMarkupFav(el));
-});
-
-//!!!!Remove favorite ---------------------------------------------------------------RESTORE
-// if (window.location.pathname === '/favorite.html') {
-//   refs.galleryList.addEventListener('click', onRemoveFavoriteBtn);
-// }
-
-console.log(favoritesInLocalStorage);
-
-if (favoritesInLocalStorage === null || favoritesInLocalStorage === []) {
-  console.log('error');
-  
-}
-
-function onRemoveFavoriteBtn(e) {
-  const removeBtn = e.target.closest(`.remove-favorite-btn`);
-  const addBtn = removeBtn.parentNode.childNodes[1];
-  console.log();
-
-  const title =
-    removeBtn.parentNode.parentNode.parentNode.childNodes[3].textContent;
-
-  if (!removeBtn) return;
-  if (!favoritesInLocalStorage) return;
-
-  if (!removeBtn.classList.contains('is-hidden')) {
-    removeBtn.classList.add('is-hidden');
-    addBtn.classList.remove('is-hidden');
   }
 
-  const indexInLocalStorage = favoritesInLocalStorage.findIndex(
-    e => e.title === title
-  );
-
-  console.log(title, indexInLocalStorage);
-
-  // localStorage.removeItem('favoriteNews');
-  favoritesInLocalStorage.splice(indexInLocalStorage, 1);
-
-  localStorage.setItem(
-    'favoritesNews',
-    JSON.stringify(favoritesInLocalStorage)
-  );
- 
-  refs.galleryList.innerHTML = '';
-  refs.errorFind.classList.remove('notfind-part-hidden');
-
   favoritesInLocalStorage.map(el => {
-    renderFavorites(refs.galleryList, createMarkupFav(el));
+    renderFavorites(favoriteGalleryList, createMarkupFav(el));
   });
-}
+
+  //!!!!Remove favorite ---------------------------------------------------------------RESTORE
+  if (window.location.pathname === '/favorite.html') {
+    favoriteGalleryList.addEventListener('click', onRemoveFavoriteBtn);
+  }
+
+  console.log(favoritesInLocalStorage);
+
+  if (favoritesInLocalStorage === null || favoritesInLocalStorage === []) {
+    console.log('error');
+  }
+
+  function onRemoveFavoriteBtn(e) {
+    const removeBtn = e.target.closest(`.remove-favorite-btn`);
+    const addBtn = removeBtn.parentNode.childNodes[1];
+    console.log();
+
+    const title =
+      removeBtn.parentNode.parentNode.parentNode.childNodes[3].textContent;
+
+    if (!removeBtn) return;
+    if (!favoritesInLocalStorage) return;
+
+    if (!removeBtn.classList.contains('is-hidden')) {
+      removeBtn.classList.add('is-hidden');
+      addBtn.classList.remove('is-hidden');
+    }
+
+    const indexInLocalStorage = favoritesInLocalStorage.findIndex(
+      e => e.title === title
+    );
+
+    console.log(title, indexInLocalStorage);
+
+    // localStorage.removeItem('favoriteNews');
+    favoritesInLocalStorage.splice(indexInLocalStorage, 1);
+
+    localStorage.setItem(
+      'favoritesNews',
+      JSON.stringify(favoritesInLocalStorage)
+    );
+
+    favoriteGalleryList.innerHTML = '';
+    refs.errorFind.classList.remove('notfind-part-hidden');
+
+    favoritesInLocalStorage.map(el => {
+      renderFavorites(favoriteGalleryList, createMarkupFav(el));
+    });
+  }
 }
