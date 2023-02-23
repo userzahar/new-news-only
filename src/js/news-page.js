369a6f542c91bbb8a2d83bce201e89dc4e0e1057
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-23 10:50:05 +0200

    Update news-page.js

--- src/js/news-page.js
+++ src/js/news-page.js
@@ -15,8 +15,6 @@ export let itemsPerPage = 8;
 export let totalPages = 0;
 
 
-
-
 if (
   window.location.pathname === '/' ||
   window.location.pathname === '/index.html'
@@ -39,7 +37,7 @@ fetchNews('/svc/mostpopular/v2/viewed/1.json', {
     itemsPerPage = 4;
   }
 
-  totalItems = data.results.length;
+  let totalItems = data.results.length;
   totalPages = Math.ceil(data.results.length / itemsPerPage);
 
   normalizePop(data.results);
