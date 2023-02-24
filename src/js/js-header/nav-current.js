4a8f6f48019f7f595fd9e33bd289efa8d19f791b
Voytesha <elflowers.info@gmail.com>
2023-02-24 03:30:46 +0200

    add function for current page

--- src/js/js-header/nav-current.js
+++ src/js/js-header/nav-current.js
@@ -1,8 +1,23 @@
-const currentPage = window.location.href;
-console.log(currentPage);
+function underlineCurrentPage() {
+  let currentPageIsFound = false;
 
-document.querySelectorAll('#navbar a').forEach(elem => {
-  if (elem.getAttribute('href').indexOf(currentPage) !== -1) {
-    elem.classList.add('navbar__link--current');
-  }
-});
+  const navLinks = document.querySelectorAll('.navbar__link');
+  console.log(navLinks);
+  const pageURL = document.URL;
+  console.log(pageURL);
+
+  navLinks.forEach(link => {
+    const isCurrentPage = pageURL.includes(link.pathname);
+    if (isCurrentPage) {
+      link.classList.add('navbar__link--current');
+      currentPageIsFound = true;
+    }
+  });
+
+  !currentPageIsFound &&
+    document
+      .querySelector('.navbar__link[href*="/index.html"]')
+      .classList.add('navbar__link--current');
+}
+
+underlineCurrentPage();
