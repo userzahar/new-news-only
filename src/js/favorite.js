e6e13a7ede1bb38b1992c20afdcdd0d2b1620e8a
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:33:07 +0300

    add favorite map

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -2,7 +2,6 @@ import './refs/refs';
 
 import './mobileMenu/mobileMenu';
 import './mobileMenu/mobileMenuCurrent';
-
 import './favorite/markap-favorite';
 import './currentPage/currentPage';
 import './darkMode/darkMode';
