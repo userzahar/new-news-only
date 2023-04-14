73af2ab1fa1dd230fad0f027399c925d77ad8a61
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:22:36 +0300

    add weather api

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -1,7 +1,6 @@
 const WEATHER_KEY = 'adbfcea561ee5005b6167772340c12ff';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
-
 async function getWeather(latitude, longitude) {
   const weatherFetch = await fetch(
     `${BASE_URL}units=metric&lat=${latitude}&lon=${longitude}&appid=${WEATHER_KEY}`
