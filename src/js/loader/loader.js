cab09975b2f41015757f27130e6674f8a1df9cdd
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:26:56 +0300

    fix loader

--- src/js/loader/loader.js
+++ src/js/loader/loader.js
@@ -4,7 +4,6 @@ function showLoader() {
   refs.loader.style.display = 'block';
   refs.body.style.overflow = 'hidden';
 }
-
 function hideLoader() {
   refs.loader.style.display = 'none';
   refs.body.style.overflow = 'visible';
