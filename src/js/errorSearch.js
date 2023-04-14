a077288f36b9eefac569dfbaf25b76727bcdc720
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:32:49 +0300

    add error

--- src/js/errorSearch.js
+++ src/js/errorSearch.js
@@ -3,7 +3,6 @@ import { refs } from './refs/refs';
 export function errorSearch() {
   const savedNews = localStorage.getItem('news');
   const parsedNews = JSON.parse(savedNews);
-
   if (parsedNews.length === 0) {
     refs.errorSearch.classList.remove('is-hidden');
     refs.favorite.classList.add('is-hidden');
