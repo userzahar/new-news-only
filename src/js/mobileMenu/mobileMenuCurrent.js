bb7ccc186287347ed4885771410a71d7abe6a8fb
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:29:11 +0300

    add current manu

--- src/js/mobileMenu/mobileMenuCurrent.js
+++ src/js/mobileMenu/mobileMenuCurrent.js
@@ -2,7 +2,6 @@ import { refs } from '../refs/refs';
 
 const arr = [refs.homePage, refs.favouritePage, refs.readPage];
 
-
 const currentPage = localStorage.getItem('currentPage') || 'home';
 
 document.querySelector(`#${currentPage}`).classList.add('mobile-menu__current');
@@ -13,36 +12,32 @@ refs.readPage.addEventListener('click', changeCurrentPageLink);
 refs.logoHomePage.addEventListener('click', changeCurrentPageLogo);
 refs.logoHomePageMenu.addEventListener('click', changeCurrentPageLogo);
 
-
-refs.menuOpentBtn.addEventListener("click", pageLoad);
+refs.menuOpentBtn.addEventListener('click', pageLoad);
 
 function changeCurrentPageLink(event) {
-    arr.forEach((item) => {
-        item.classList.remove('mobile-menu__current');
-    })
-    event.currentTarget.classList.add('mobile-menu__current');
- localStorage.setItem('currentPage', event.currentTarget.id); 
-    
-};
+  arr.forEach(item => {
+    item.classList.remove('mobile-menu__current');
+  });
+  event.currentTarget.classList.add('mobile-menu__current');
+  localStorage.setItem('currentPage', event.currentTarget.id);
+}
 
 function changeCurrentPageLogo() {
-    arr.forEach((item) => {
-        item.classList.remove('mobile-menu__current');
-    })
+  arr.forEach(item => {
+    item.classList.remove('mobile-menu__current');
+  });
 
-    refs.homePage.classList.add('mobile-menu__current');
-    localStorage.setItem('currentPage', refs.homePage.id);
+  refs.homePage.classList.add('mobile-menu__current');
+  localStorage.setItem('currentPage', refs.homePage.id);
 }
- 
-function pageLoad() { 
-    const currentPageName = window.location.pathname;
+
+function pageLoad() {
+  const currentPageName = window.location.pathname;
   if (currentPageName === '/index.html') {
     localStorage.setItem('currentPage', refs.homePage.id);
-    }
-    else if(currentPageName === '/favorite.html'){
-    localStorage.setItem('currentPage', refs.favouritePage.id);      
-    }
-  else if(currentPageName === '/read.html'){
-      localStorage.setItem('currentPage', refs.readPage.id);
-    }
+  } else if (currentPageName === '/favorite.html') {
+    localStorage.setItem('currentPage', refs.favouritePage.id);
+  } else if (currentPageName === '/read.html') {
+    localStorage.setItem('currentPage', refs.readPage.id);
+  }
 }
