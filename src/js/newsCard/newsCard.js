f72a9cb5d38a01721b28d4e48860465660c7d588
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-14 20:29:31 +0300

    add newCard

--- src/js/newsCard/newsCard.js
+++ src/js/newsCard/newsCard.js
@@ -2,6 +2,5 @@ import { btnAddToFavorite } from '../favorite/addToFavorite';
 import { linkReadMore } from '../btns/readMore';
 
 const newsContainer = document.querySelector('.news > .container');
-
 newsContainer.addEventListener('click', btnAddToFavorite);
 newsContainer.addEventListener('click', linkReadMore);
